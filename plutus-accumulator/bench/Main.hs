c3667fa975c1408fd27854490dab62b0b0c3b7e1
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2025-12-03 15:45:31 +0100

    Bump plutus to 1.55 and disable benchmarks for now

--- plutus-accumulator/bench/Main.hs
+++ plutus-accumulator/bench/Main.hs
@@ -1,11 +1,12 @@
 module Main where
 
-import Accumulator.Run (runCheckMembership, runCheckNonMembership)
-import Bls.Run (runBls)
+-- import Accumulator.Run (runCheckMembership, runCheckNonMembership)
+-- import Bls.Run (runBls)
 import System.IO (stdout)
 
 main :: IO ()
 main = do 
-    runBls stdout
-    runCheckMembership stdout
-    runCheckNonMembership stdout
+  print "plutus-accumulator"
+    -- runBls stdout
+    -- runCheckMembership stdout
+    -- runCheckNonMembership stdout
